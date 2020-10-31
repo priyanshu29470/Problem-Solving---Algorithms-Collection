4509999c77b363362cbc6f1642908fa5083ad4f1
Mayur Kumawat <33410113+Mayurkumawat012@users.noreply.github.com>
2020-10-31 20:46:26 +0530

    Create addFiveNum.py

---
+++ addFiveNum.py
@@ -0,0 +1,13 @@
+# This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+num3 = 4.1
+num4 = 5.1
+num5 = 6.1
+
+# Add two numbers
+sum = num1 + num2 + num3 + num4 + num5
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, num3, num4, num5, sum))
